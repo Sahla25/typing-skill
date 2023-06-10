120bf4517c6b411f52f362bdeb6ad663f9d23cf8
Sahla25 <92774476+Sahla25@users.noreply.github.com>
2023-06-10 16:45:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useMemo } from "react";
+
+import { Knob } from 'primereact/knob';
+import { InputText } from "primereact/inputtext";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [alphabet, setAlphabet] = useState("");
+  const [wordCount, setWordCount] = useState(0);
+  const [timer, setTimer] = useState(10);
+
+  useEffect(() => {
+    const words = alphabet.split(" ");
+    setWordCount(words.length);
+  }, [alphabet]);
+
+  useMemo(() => {
+    let settime = 100;
+    const interval = setInterval(() => {
+      settime = 10;
+      if (settime === 0) {
+        clearInterval(interval); // Pass the interval identifier to clearInterval
+      }
+      setTimer(settime);
+    }, 2000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div align="center">
+      <div className="">
+        <label htmlFor="" style={{ color: "darkblue"}} className='font-bold text-6x1'>WAIT and TYPE here</label>
+        <Knob valueColor={"lightbrown"} rangeColor={"darkgreen"} className='mt-4' value={timer} size={200} min={0} max={59} readOnly />
+        <h3 className='text-primary m-0 p-0'>Words Count: <span style={{ color: "darkbrown" }}>{wordCount}</span></h3>
+        <InputText className='mt-4' value={alphabet} onChange={(e) => setAlphabet(e.target.value)} />
+      </div>
     </div>
   );
 }
